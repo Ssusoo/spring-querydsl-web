e0649f90242525c3a84ec2f265e6d2c0ab35c72d
김수환 <zzanggoon8@hiconsy.com>
2022-06-21 18:07:23 +0900

    etc

---
+++ src/main/java/me/ssu/springquerydslweb/SpringQuerydslWebApplication.java
@@ -0,0 +1,13 @@
+package me.ssu.springquerydslweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuerydslWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuerydslWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ssu/springquerydslweb/SpringQuerydslWebApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ssu.springquerydslweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuerydslWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
