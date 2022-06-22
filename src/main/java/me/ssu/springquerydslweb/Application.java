8b206c4cc36898db887428320247e8ee5441506e
김수환 <zzanggoon8@hiconsy.com>
2022-06-22 18:35:22 +0900

    etc

--- src/main/java/me/ssu/springquerydslweb/Application.java
+++ src/main/java/me/ssu/springquerydslweb/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringQuerydslWebApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringQuerydslWebApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
