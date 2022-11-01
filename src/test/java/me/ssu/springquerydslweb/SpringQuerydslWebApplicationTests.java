c5e015582a1f987cff6360287e9d633c5f33e2de
김수환 <zzanggoon8@hiconsy.com>
2022-11-01 17:51:29 +0900

    Querydsl Setting

--- src/test/java/me/ssu/springquerydslweb/SpringQuerydslWebApplicationTests.java
+++ src/test/java/me/ssu/springquerydslweb/SpringQuerydslWebApplicationTests.java
@@ -1,13 +1,22 @@
 package me.ssu.springquerydslweb;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringQuerydslWebApplicationTests {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+class SpringQuerydslWebApplicationTests extends BaseTest{
+
 
 	@Test
-	void contextLoads() {
+	@DisplayName("회원가입 뷰")
+	void signUpForm() throws Exception {
+		mockMvc.perform(get("/sign-up"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("account/sign-up"))
+		;
 	}
-
 }
